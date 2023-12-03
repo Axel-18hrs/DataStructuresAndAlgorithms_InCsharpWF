d540669bd43759471709f7c9ff75159a2e920a9d
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-12-03 17:05:10 -0600

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,66 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
+            lblBeggininMessage = new Label();
             btnGraphs = new Button();
             btnTrees = new Button();
             btnQueues = new Button();
             btnLists = new Button();
             btnStacks = new Button();
-            lblBeggininMessage = new Label();
-            panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // panel1
+            // lblBeggininMessage
             // 
-            panel1.Controls.Add(lblBeggininMessage);
-            panel1.Controls.Add(btnGraphs);
-            panel1.Controls.Add(btnTrees);
-            panel1.Controls.Add(btnQueues);
-            panel1.Controls.Add(btnLists);
-            panel1.Controls.Add(btnStacks);
-            panel1.Location = new Point(8, 8);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(1107, 507);
-            panel1.TabIndex = 0;
+            lblBeggininMessage.AutoSize = true;
+            lblBeggininMessage.Font = new Font("Yu Gothic UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblBeggininMessage.Location = new Point(399, 106);
+            lblBeggininMessage.Name = "lblBeggininMessage";
+            lblBeggininMessage.Size = new Size(329, 60);
+            lblBeggininMessage.TabIndex = 11;
+            lblBeggininMessage.Text = "Data structures";
             // 
             // btnGraphs
             // 
             btnGraphs.FlatStyle = FlatStyle.System;
-            btnGraphs.Location = new Point(466, 368);
+            btnGraphs.Location = new Point(477, 380);
             btnGraphs.Name = "btnGraphs";
             btnGraphs.Size = new Size(160, 41);
-            btnGraphs.TabIndex = 4;
+            btnGraphs.TabIndex = 10;
             btnGraphs.Text = "Graphs";
             btnGraphs.UseVisualStyleBackColor = true;
             // 
             // btnTrees
             // 
             btnTrees.FlatStyle = FlatStyle.System;
-            btnTrees.Location = new Point(466, 321);
+            btnTrees.Location = new Point(477, 333);
             btnTrees.Name = "btnTrees";
             btnTrees.Size = new Size(160, 41);
-            btnTrees.TabIndex = 3;
+            btnTrees.TabIndex = 9;
             btnTrees.Text = "Trees";
             btnTrees.UseVisualStyleBackColor = true;
             // 
             // btnQueues
             // 
             btnQueues.FlatStyle = FlatStyle.System;
-            btnQueues.Location = new Point(466, 274);
+            btnQueues.Location = new Point(477, 286);
             btnQueues.Name = "btnQueues";
             btnQueues.Size = new Size(160, 41);
-            btnQueues.TabIndex = 2;
+            btnQueues.TabIndex = 8;
             btnQueues.Text = "Queues";
             btnQueues.UseVisualStyleBackColor = true;
             // 
             // btnLists
             // 
             btnLists.FlatStyle = FlatStyle.System;
-            btnLists.Location = new Point(466, 180);
+            btnLists.Location = new Point(477, 192);
             btnLists.Name = "btnLists";
             btnLists.Size = new Size(160, 41);
-            btnLists.TabIndex = 0;
+            btnLists.TabIndex = 6;
             btnLists.Text = "Lists";
             btnLists.UseVisualStyleBackColor = true;
             btnLists.Click += btnLists_Click;
@@ -95,22 +90,12 @@
             // btnStacks
             // 
             btnStacks.FlatStyle = FlatStyle.System;
-            btnStacks.Location = new Point(466, 227);
+            btnStacks.Location = new Point(477, 239);
             btnStacks.Name = "btnStacks";
             btnStacks.Size = new Size(160, 41);
-            btnStacks.TabIndex = 1;
+            btnStacks.TabIndex = 7;
             btnStacks.Text = "Stacks";
             btnStacks.UseVisualStyleBackColor = true;
-            // 
-            // lblBeggininMessage
-            // 
-            lblBeggininMessage.AutoSize = true;
-            lblBeggininMessage.Font = new Font("Yu Gothic UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblBeggininMessage.Location = new Point(388, 94);
-            lblBeggininMessage.Name = "lblBeggininMessage";
-            lblBeggininMessage.Size = new Size(329, 60);
-            lblBeggininMessage.TabIndex = 5;
-            lblBeggininMessage.Text = "Data structures";
             // 
             // Form1
             // 
@@ -118,26 +103,29 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(224, 224, 224);
             ClientSize = new Size(1127, 527);
-            Controls.Add(panel1);
+            Controls.Add(lblBeggininMessage);
+            Controls.Add(btnGraphs);
+            Controls.Add(btnTrees);
+            Controls.Add(btnQueues);
+            Controls.Add(btnLists);
+            Controls.Add(btnStacks);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             Load += Form1_Load;
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Panel panel1;
-        private Button btnLists;
-        private Button btnQueues;
-        private Button btnStacks;
+        private Label lblBeggininMessage;
         private Button btnGraphs;
         private Button btnTrees;
-        private Label lblBeggininMessage;
+        private Button btnQueues;
+        private Button btnLists;
+        private Button btnStacks;
     }
 }
